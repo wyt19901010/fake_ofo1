4bf18c13818ee6bab146e0cde922ac0cbe742dd3
WU YUTONG <wyt363466854@gmail.com>
2018-03-30 13:45:26 +0900

    Initial Commit

---
+++ fake_ofo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fake_ofo
+//
+//  Created by YUTONG WU on 2018/3/30.
+//  Copyright © 2018年 Big. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
